dded600b075bbb750e959c3b25f0f06e74798ee7
Adrian <kontakt@antara.pl>
2025-08-12 16:31:06 -0700

    refactor unzip

--- src/unzip.ts
+++ src/unzip.ts
@@ -2,41 +2,84 @@ import yauzl from 'yauzl';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export async function unzipFile(filePath: string, outputDir: string, deleteZip: boolean = true): Promise<void> {
-    return new Promise((resolve, reject) => {
-        yauzl.open(filePath, {lazyEntries: true}, function(err, zipfile) {
-            if (err) {
-                return reject(err);
-            }
-            zipfile.readEntry();
-            zipfile.on("entry", function(entry) {
-                if (entry.fileName.endsWith('/')) {
-                    // Directory file names end with '/'.
-                    // Note that entries for directories themselves are optional.
-                    // An entry's fileName implicitly requires its parent directories to exist.
-                    zipfile.readEntry();
-                } else {
-                    // file entry
-                    zipfile.openReadStream(entry, function(err, readStream) {
-                    if (err) {
-                        return reject(err);
-                    }
-                    readStream.on("end", function() {
-                        zipfile.readEntry();
-                    });
-                    readStream.pipe(fs.createWriteStream(path.join(outputDir, entry.fileName)));
-                  });
-                }
-            });
-            zipfile.on("close", async function() {
-              if (deleteZip) {
-                await fs.promises.unlink(filePath).catch(() => {}); // delete zip file after extraction, no harm if it fails
-              }
-              resolve();
-            });
-            zipfile.on("error", function(err) {
-                return reject(err);
-            });
-        });
+function mkdirp(dir: string, cb: () => void) {
+  if (dir === ".") return cb();
+  fs.stat(dir, function(err) {
+    if (err == null) return cb(); // already exists
+
+    const parent = path.dirname(dir);
+    mkdirp(parent, function() {
+      process.stdout.write(dir.replace(/\/$/, "") + "/\n");
+      fs.mkdir(dir, cb);
     });
+  });
+}
+
+export async function unzipFile(filePath: string, outputDir: string, deleteZip: boolean = true): Promise<void> {
+  return new Promise((resolve, reject) => {
+    yauzl.open(filePath, {lazyEntries: true}, function(err, zipfile) {
+      if (err) return reject(err);
+
+            // track when we've closed all our file handles
+      let handleCount = 0;
+      function incrementHandleCount() {
+        handleCount++;
+      }
+      function decrementHandleCount() {
+        handleCount--;
+        if (handleCount === 0) {
+          // all input and output handles closed
+          resolve()
+        }
+      }
+
+      incrementHandleCount();
+      zipfile.on("close", async function() {
+        if (deleteZip) {
+          await fs.promises.unlink(filePath).catch(() => {}); // delete zip file after extraction, no harm if it fails
+        }
+        decrementHandleCount();
+      });
+
+      zipfile.readEntry();
+
+      zipfile.on("entry", function(entry) {
+        if (entry.fileName.endsWith('/')) {
+          // Directory file names end with '/'.
+          // Note that entries for directories themselves are optional.
+          // An entry's fileName implicitly requires its parent directories to exist.
+          mkdirp(entry.fileName, function() {
+            if (err) return reject(err);
+            zipfile.readEntry();
+          });
+        } else {
+          mkdirp(path.dirname(entry.fileName), function() {
+            zipfile.openReadStream(entry, function(err, readStream) {
+              if (err) return reject(err);
+
+              // console.log('Extracting', entry.fileName);
+
+              readStream.on("error", function(err) {
+                return reject(err);
+              });
+
+              readStream.on("end", function() {
+                  zipfile.readEntry();
+              });
+
+              // pump file contents
+              const writeStream = fs.createWriteStream(path.join(outputDir, entry.fileName));
+              incrementHandleCount();
+              writeStream.on("close", decrementHandleCount);
+              readStream.pipe(writeStream);
+            });
+          });
+        }
+      });
+
+      zipfile.on("error", function(err) {
+        return reject(err);
+      });
+    });
+  });
 }
